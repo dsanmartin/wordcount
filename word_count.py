82651877d6be3d3a6b1cae6d6d20e8490ae5473e
Daniel San Martin <dsanmartinreyes@gmail.com>
2017-01-04 11:30:10 -0300

    Comentario para el futuro

--- word_count.py
+++ word_count.py
@@ -1,7 +1,7 @@
 #happy = "I felt happy because I saw the others were happy and because I knew I should feel happy but I was not really happy"
 happy = input("Enter a statement to word count: ")
 
-words = happy.split()
+words = happy.split() # split user input on spaces
 
 counts = {}
 for word in words:
